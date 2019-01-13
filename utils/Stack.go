42337b08ba015a35d03e6ae86d7f37c36bfd1cae
Nemanja <nemanja.vasic@codecentric.de>
2019-01-13 12:10:06 +0100

    Add Stack implementation

---
+++ utils/Stack.go
@@ -0,0 +1,19 @@
+package utils
+
+type Stack struct {
+	slice []int
+}
+
+func (stack *Stack) Push(item int) {
+	stack.slice = append(stack.slice, item)
+}
+
+func (stack *Stack) Peek() int {
+	return stack.slice[len(stack.slice)-1]
+}
+
+func (stack *Stack) Pop() int {
+	topItem := stack.Peek()
+	stack.slice = stack.slice[0 : len(stack.slice)-1]
+	return topItem
+}
